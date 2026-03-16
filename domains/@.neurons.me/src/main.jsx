5085ee6280f08583d8cfe449d4338e7435a31c37
neurons.me <admin@neurons.me>
2026-03-16 16:55:16 -0600

    -
    -

--- domains/@.neurons.me/src/main.jsx
+++ domains/@.neurons.me/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react';
+import { Fragment, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './i18n';
+import '../../../this/GUI/npm/dist/styles.css';
+import '../../../this/GUI/npm/dist/material-symbols.css';
 import { GUI } from 'all.this';
-const { GUIProvider } = GUI;
 import App from './App.jsx';
+const RootProvider = GUI.Theme ?? GUI.GuiProvider ?? GUI.GUIProvider ?? Fragment;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <GUIProvider>
+  <RootProvider>
   <App />
-  </GUIProvider>
+  </RootProvider>
   </StrictMode>
 );
